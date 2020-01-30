16681bbd99b7543e07c0809c25809a1b884f25d2
Georgy <goosebumpsmail2@gmail.com>
2020-01-31 01:44:31 +0300

    Сделал ядро менеджера, добавил функции добавления, просмотра и удаления

---
+++ Daemon.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace Daemon\Main;
+
+define('filename', 'daemonList.xml');
+
+
+class Daemon
+{
+
+    private function _startDaemon($daemonName, $comment)
+    {
+        $pid = exec("nohup php -f " . $daemonName . " > /dev/null 2>&1 & echo $!", $output);
+        echo "Демон запущен" . PHP_EOL;
+        echo "PID: " . $pid . PHP_EOL;
+
+        $this->_addXmlDoc($pid, $daemonName, $comment);
+    }
+
+    private function _showAllDaemons()
+    {
+        $daemonXml = new \SimpleXMLElement(file_get_contents(filename));
+
+        for ($i = 0; $i < $daemonXml->count(); $i++)
+        {
+            echo "------------------------------------------------" . "\n";
+            echo "PID - \"" . $daemonXml->item[$i]->pid . "\" | ";
+            echo "Command - \"" . $daemonXml->item[$i]->command . "\" | ";
+            echo "Comment - \"" . $daemonXml->item[$i]->comment . '" |' . PHP_EOL . PHP_EOL;
+        }
+
+
+    }
+
+    private function _killDaemon($pid)
+    {
+        echo exec('kill ' . $pid) . PHP_EOL;
+        echo "Процесс с номером " . $pid . ' удален' . PHP_EOL;
+
+        $this->_deleteXmlDoc($pid);
+    }
+
+    private function _addXmlDoc($pid, $daemonName, $comment)
+    {
+        $daemonXml = new \SimpleXMLElement(file_get_contents(filename));
+        $daemonXml->addChild('item');
+        $daemonXml->item[$daemonXml->count() - 1]->addChild('pid', $pid);
+        $daemonXml->item[$daemonXml->count() - 1]->addChild('command', "php -f " . $daemonName);
+        $daemonXml->item[$daemonXml->count() - 1]->addChild('comment', $comment);
+        file_put_contents(filename, $daemonXml->asXML());
+
+    }
+
+    private function _deleteXmlDoc($pid)
+    {
+        $daemonXml = new \SimpleXMLElement(file_get_contents(filename));
+        for ($i = 0; $i < $daemonXml->count(); $i++)
+            if ($daemonXml->item[$i]->pid == $pid)
+                unset($daemonXml->item[$i]);
+
+        file_put_contents(filename, $daemonXml->asXML());
+
+    }
+
+
+}
